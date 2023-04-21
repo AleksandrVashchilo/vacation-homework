69fa97f801a42c5cb542d663a63abe515e5dda95
AleksandrVashchilo <suszycki.alexander@gmail.com>
2023-04-21 17:35:15 +0200

    First commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,23 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        int product = 0;
+        try {
+            int countOfPeople = scanner.nextInt();
+            int countOfDays = scanner.nextInt();
+
+            Vacation vacation = new Vacation(countOfPeople, countOfDays);
+            product = vacation.calculate();
+        }
+        catch (InputMismatchException exception) {
+            System.out.println("Вы ввели неравильные данные. Введите число");
+        }
+        System.out.println(product);
     }
 }
--- src/main/java/org/example/Vacation.java
+++ src/main/java/org/example/Vacation.java
@@ -1,2 +1,16 @@
-package org.example;public class Vacation {
+package org.example;
+
+public class Vacation {
+
+    private int countOfPeople;
+    private int countOfDays;
+
+    public int calculate() {
+        return countOfDays * countOfPeople;
+    }
+
+    public Vacation(int countOfPeople, int countOfDays) {
+        this.countOfPeople = countOfPeople;
+        this.countOfDays = countOfDays;
+    }
 }
